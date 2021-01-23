bcc4d207bd962b347f61fb50ae78bcb57620e924
adamoudad <adam.oudad@gmail.com>
2021-01-23 14:49:42 +0900

    fish: Add py abbrev

--- fish/config.fish
+++ fish/config.fish
@@ -206,6 +206,7 @@ abbr -a -- 'vnc-spectate' 'x0vncserver -rfbauth ~/.vnc/passwd -AcceptKeyEvents=o
 abbr -a -- 'vnc-control' 'x0vncserver -rfbauth ~/.vnc/passwd'
 abbr -a -- emacsd 'emacs --daemon'
 abbr -a -- whatsmyip 'curl ifconfig.me'
+abbr -a -- py 'python'
 # abbr -a -- run-help man
 # abbr -a -- which-command whence
 # abbr -a -- xmodmap-revert 'xmodmap ~/Git/dot-files/.Xmodmap-revert'
